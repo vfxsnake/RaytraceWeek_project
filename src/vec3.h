65d584ed2b97afd727b0dcefb3443ae27033ac74
vfxnake <jazzjoe@gmail.com>
2023-08-29 22:05:53 -0400

    vector utils implemented

--- src/vec3.h
+++ src/vec3.h
@@ -60,4 +60,61 @@ public:
     }
 };
 
-// TODO: add vector utility functions.
+// creating an alias for vec3: point, so it is clear when using a point for clarity in the code
+using point3 = vec3;
+
+// inline utility functions for increase the performance by avoiding the overhead of the function call.
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v)
+{
+    return out << v.components[0] << ' ' << v.components[0] << ' ' << v.components[0];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.components[0] + v.components[0], u.components[1] + v.components[1], u.components[2] + v.components[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.components[0] - v.components[0], u.components[1] - v.components[2], u.components[2] - v.components[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.components[0] * v.components[0], u.components[1] * v.components[1], u.components[2] * v.components[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return vec3(t * v.components[0], t * v.components[1], t * v.components[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) // TODO: once implemented and tested try to add const vec3 &v to see if it works...
+{
+    return (1/t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.components[0] * v.components[0] +
+           u.components[1] * v.components[1] +
+           u.components[2] * v.components[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.components[1] * v.components[2] - u.components[2] * v.components[1],
+                u.components[2] * v.components[0] - u.components[0] * v.components[2],
+                u.components[0] * v.components[1] - u.components[1] * v.components[0]);
+}
+
+inline vec3 unit_vector(vec3 v)
+{
+    return v / v.length();
+}
+
