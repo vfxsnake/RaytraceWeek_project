2d48dbd46dfd643f3508580a041de1a373b86f60
vfxnake <jazzjoe@gmail.com>
2023-08-30 23:51:48 -0400

    camera and ray class

--- src/color.h
+++ src/color.h
@@ -7,7 +7,7 @@ using color = vec3;
 
 void write_color(std::ostream &out, color pixel_color)
 {
-    out << static_cast<int>(255.999 * pixel_color.get_x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.get_y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.get_z()) << '\n';
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
--- src/vec3.h
+++ src/vec3.h
@@ -4,117 +4,111 @@
 #include <cmath>
 #include <iostream>
 
-class vec3
+using std::sqrt;
+
+class vec3 
 {
-public:
+  public:
+    double e[3];
 
-    double components[3];
-    
-    // constructors
-    vec3() : components{0,0,0}{};
-    vec3(double x, double y, double z) : components{x,y,z}{}; 
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-    double get_x() const {return components[0];}
-    double get_y() const {return components[1];}
-    double get_z() const {return components[2];}
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
 
-    // operator over loads
-    vec3 operator-() const { return vec3(-components[0], -components[1], -components[2]); }
-    double operator[](int i) const {return components[i];}
-    double& operator[](int i) {return components[i];}
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
 
-    vec3& operator+=(const vec3 &v)
-    {
-        components[0] += v[0];
-        components[1] += v[1];
-        components[2] += v[2];
+    vec3& operator+=(const vec3 &v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
         return *this;
     }
 
-    vec3& operator*=(double t)
+    vec3& operator*=(double t) 
     {
-        components[0] *= t;
-        components[1] *= t;
-        components[1] *= t;
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t)
+    vec3& operator/=(double t) 
     {
-        return *this *= 1.0/t;
+        return *this *= 1/t;
     }
 
-
-    double length2() const
+    double length() const 
     {
-        return (
-                components[0] * components[0] +
-                components[1] * components[1] +
-                components[2] * components[2]
-                );
+        return sqrt(length_squared());
     }
 
-    double length() const
+    double length_squared() const 
     {
-        return std::sqrt(length2());
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 };
 
-// creating an alias for vec3: point, so it is clear when using a point for clarity in the code
+// point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
 
-// inline utility functions for increase the performance by avoiding the overhead of the function call.
-inline std::ostream& operator<<(std::ostream &out, const vec3 &v)
+
+// Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) 
 {
-    return out << v.components[0] << ' ' << v.components[0] << ' ' << v.components[0];
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3 &u, const vec3 &v)
+inline vec3 operator+(const vec3 &u, const vec3 &v) 
 {
-    return vec3(u.components[0] + v.components[0], u.components[1] + v.components[1], u.components[2] + v.components[2]);
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &u, const vec3 &v)
+inline vec3 operator-(const vec3 &u, const vec3 &v) 
 {
-    return vec3(u.components[0] - v.components[0], u.components[1] - v.components[2], u.components[2] - v.components[2]);
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &u, const vec3 &v)
+inline vec3 operator*(const vec3 &u, const vec3 &v) 
 {
-    return vec3(u.components[0] * v.components[0], u.components[1] * v.components[1], u.components[2] * v.components[2]);
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v)
+inline vec3 operator*(double t, const vec3 &v) 
 {
-    return vec3(t * v.components[0], t * v.components[1], t * v.components[2]);
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t)
+inline vec3 operator*(const vec3 &v, double t) 
 {
     return t * v;
 }
 
-inline vec3 operator/(vec3 v, double t) // TODO: once implemented and tested try to add const vec3 &v to see if it works...
+inline vec3 operator/(vec3 v, double t) 
 {
     return (1/t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v)
+inline double dot(const vec3 &u, const vec3 &v) 
 {
-    return u.components[0] * v.components[0] +
-           u.components[1] * v.components[1] +
-           u.components[2] * v.components[2];
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3 &u, const vec3 &v)
+inline vec3 cross(const vec3 &u, const vec3 &v) 
 {
-    return vec3(u.components[1] * v.components[2] - u.components[2] * v.components[1],
-                u.components[2] * v.components[0] - u.components[0] * v.components[2],
-                u.components[0] * v.components[1] - u.components[1] * v.components[0]);
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v)
+inline vec3 unit_vector(vec3 v) 
 {
     return v / v.length();
 }
-
