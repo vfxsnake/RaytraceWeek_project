0a81122109515e2cc1bdf13b4234373179bea445
vfxnake <jazzjoe@gmail.com>
2023-09-12 22:44:45 -0400

    out color normal correct for multiple spheres

--- src/sphere.h
+++ src/sphere.h
@@ -31,6 +31,7 @@ public:
         }
         
         rec.t  = root;
+        rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
         
